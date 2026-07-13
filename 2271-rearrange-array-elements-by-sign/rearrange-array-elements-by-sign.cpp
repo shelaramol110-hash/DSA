b37206bb0bb7ad0934f264debc9a1abe1958eb1e
shelaramol110-hash <shelaramol110@gmail.com>
2026-07-13 23:54:49 +0530

    Time: 4 ms (77.54%) | Memory: 128.4 MB (34.32%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> ans(n) ;
+        
+        vector<int> ans(nums.size()) ;
         int pos = 0 ;
         int neg  = 1;
         
-        for(int i = 0 ; i <n;i++){
+        for(int i = 0 ; i <nums.size();i++){
             
             if(nums[i]>=0){
                 ans[pos]= nums[i];
